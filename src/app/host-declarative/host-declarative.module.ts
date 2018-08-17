87bcadc60813e81b675ac47c935813bdd20e136e
jsonberry <jason.s.awbrey@gmail.com>
2018-08-17 14:08:55 -0700

    and another stackblitz update oops

--- src/app/host-declarative/host-declarative.module.ts
+++ src/app/host-declarative/host-declarative.module.ts
@@ -1,10 +1,10 @@
-import {ViewOneModule} from '@/view-one'
-import {ViewThreeModule} from '@/view-three'
-import {ViewTwoModule} from '@/view-two'
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {ContainerStateModule} from '../container-state/container-state.module'
+import {ViewOneModule} from '../view-one'
+import {ViewThreeModule} from '../view-three'
+import {ViewTwoModule} from '../view-two'
 import {HostDeclarativeComponent} from './host-declarative.component'
 
 @NgModule({
